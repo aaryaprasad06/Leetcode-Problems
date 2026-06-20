03f43d963d910b6d388992236559e2d2ae478d76
Aarya Prasad <prasadaarya740@gmail.com>
2026-06-20 18:00:05 +0530

    Time: 12 ms (55.95%) | Memory: 19.3 MB (68.56%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -7,8 +7,7 @@ class Solution:
         for right in range(len(s)):
             while s[right] in seen:
                 seen.remove(s[left])
-                left+=1 
-
-            seen.add(s[right]) 
-            ans= max(ans, right-left+1)
+                left+=1
+            seen.add(s[right])
+            ans=max(ans, right-left+1)
         return ans
