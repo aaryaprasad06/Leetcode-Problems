c0d48e23070a4011b5fb56b2c29a155fbb49b53f
Aarya Prasad <prasadaarya740@gmail.com>
2026-06-20 18:09:51 +0530

    Time: 19 ms (21.19%) | Memory: 23.7 MB (31.47%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,10 +4,11 @@ class Solution:
 
         for s in strs:
             alpha=[0]*26
-            for char in s:
-                alpha[ord(char)- ord('a')]+=1 
-            key= tuple(alpha)
+            for i in s:
+                alpha[ord(i)-ord('a')]+=1
+            key=tuple(alpha)
             if key not in hashmap:
-                hashmap[key]= []
-            hashmap[key].append(s)
+                hashmap[key]=[s]
+            else:
+                hashmap[key].append(s)
         return list(hashmap.values())
