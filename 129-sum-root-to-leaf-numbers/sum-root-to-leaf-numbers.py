c8cd6e0c06dffab22536308d41ac6ed8dd31733a
Aarya Prasad <prasadaarya740@gmail.com>
2026-06-22 16:10:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (30.77%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -8,21 +8,16 @@ class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         if not(root.left) and not(root.right):
             return root.val
-        num= root.val
         def solve(root, current):
             if not root:
                 return 0
             current= current* 10+ root.val
             if not root.left and not root.right:
                 return current
-            if root.left and not root.right:
-                return solve(root.left, current)
-            elif root.right and not root.left:
-                return solve(root.right, current)
-            else:
-                return solve(root.left, current)+ solve(root.right, current)
+            return solve(root.left, current)+ solve(root.right, current)
+        return solve(root, 0)
 
 
-        return solve(root.left, num) + solve(root.right, num)
+       
         
         
