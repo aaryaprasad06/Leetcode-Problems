b0bf7d3813c9fc06a8ce0757b80e88bfcd878846
Aarya Prasad <prasadaarya740@gmail.com>
2026-07-15 20:05:27 +0530

    Time: 41 ms (86.18%) | Memory: 19.2 MB (18.24%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,17 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-        memory= {}
+        steps=[]
+        steps.append(0)
+        steps.append(1)
 
-        def helper(n):
-            if n == 0: return 0
-            if n == 1: return 1 
-            
-            if n in memory:
-                return memory[n]
-            else:
-                result = helper(n-1) + helper(n-2)
-                memory[n] = result
-            
-            return result
+        for i in range(2, n+1):
+            steps.append(steps[i-1] + steps[i-2])
         
-        return helper(n)
+        return steps[n]
