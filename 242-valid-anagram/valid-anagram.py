3dcddf86a4d436eb94907b050c067571ded22273
Aarya Prasad <prasadaarya740@gmail.com>
2026-09-02 19:36:46 +0530

    Time: 11 ms (77.32%) | Memory: 19.4 MB (76.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)== sorted(t)
+        alpha1= [0]* 26 
+        alpha2= [0]* 26 
+        for ch in s:
+            idx= ord(ch)- ord('a')
+            alpha1[idx]+=1 
+        
+        for ch in t:
+            idx= ord(ch)- ord('a')
+            alpha2[idx]+=1 
+        
+        return alpha1==alpha2
